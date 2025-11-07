0a85c1cc4e2dca0fd921943a9fe8bd99f298db8f
GumSurf <gabriel.christe@gmail.com>
2025-11-07 16:58:10 +0100

    Add: SeoHelmet

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ import Footer from "./components/Footer";
 import GoogleReviewsFeaturable from "./components/GoogleReviewFeaturable";
 import SectionDivider from "./components/SectionDivider";
 import InstagramGrid from "./components/InstagramGrid";
+import SeoHelmet from "./components/SeoHelmet";
 
 function App() {
   return (
     <div className="relative bg-(--color-background) text-(--color-text) min-h-screen">
-      
+      <SeoHelmet
+        title="AgCoaching – Coaching sportif personnalisé"
+        description="AgCoaching propose du coaching sportif, préparation physique et accompagnement personnalisé à Lorient et Hennebont."
+        image="/images/hero-agcoaching.jpg"
+      />
+
       <Hero />
       <Services />
       <Partners />
